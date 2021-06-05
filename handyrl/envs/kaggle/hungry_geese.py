3644d1efac7f4716940c91e9f03684f6504508ce
shkim960520 <shkim960520@gmail.com>
2021-06-05 12:35:55 +0900

    TorusConv2d uses torch circular padding

--- handyrl/envs/kaggle/hungry_geese.py
+++ handyrl/envs/kaggle/hungry_geese.py
@@ -21,16 +21,13 @@ from ...environment import BaseEnvironment
 
 
 class TorusConv2d(nn.Module):
-    def __init__(self, input_dim, output_dim, kernel_size, bn):
+    def __init__(self, input_dim, output_dim, kernel_size, padding, bn):
         super().__init__()
-        self.edge_size = (kernel_size[0] // 2, kernel_size[1] // 2)
-        self.conv = nn.Conv2d(input_dim, output_dim, kernel_size=kernel_size)
+        self.conv = nn.Conv2d(input_dim, output_dim, kernel_size=kernel_size, padding=padding, padding_mode='circular')
         self.bn = nn.BatchNorm2d(output_dim) if bn else None
 
     def forward(self, x):
-        h = torch.cat([x[:,:,:,-self.edge_size[1]:], x, x[:,:,:,:self.edge_size[1]]], dim=3)
-        h = torch.cat([h[:,:,-self.edge_size[0]:], h, h[:,:,:self.edge_size[0]]], dim=2)
-        h = self.conv(h)
+        h = self.conv(x)
         h = self.bn(h) if self.bn is not None else h
         return h
 
@@ -40,8 +37,8 @@ class GeeseNet(nn.Module):
         super().__init__()
         layers, filters = 12, 32
 
-        self.conv0 = TorusConv2d(17, filters, (3, 3), True)
-        self.blocks = nn.ModuleList([TorusConv2d(filters, filters, (3, 3), True) for _ in range(layers)])
+        self.conv0 = TorusConv2d(17, filters, (5, 5), 2, True)
+        self.blocks = nn.ModuleList([TorusConv2d(filters, filters, (3, 3), 1, True) for _ in range(layers)])
         self.head_p = nn.Linear(filters, 4, bias=False)
         self.head_v = nn.Linear(filters * 2, 1, bias=False)
 
